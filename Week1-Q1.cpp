0f4bc13b824d6059d3babdcdd62094af95c5a1b3
Devik Gupta <64042688+guptadevik2003@users.noreply.github.com>
2024-01-29 09:21:01 +0530

    Create Week1-Q1.cpp

---
+++ Week1-Q1.cpp
@@ -0,0 +1,27 @@
+// 29-JAN-2024 Week 1 Lab Manual Q1
+
+#include <iostream>
+using namespace std;
+
+int LinearSearch(int arr[], int len, int key){
+    for(int i=0; i<len; i++){
+        if(arr[i] == key)
+            return i;
+    }
+    return -1;
+}
+
+int main(){
+    int iter, len, key, res;
+    cin>>iter;
+    for(int i=0; i<iter; i++){
+        cin>>len;
+        int arr[len];
+        for(int j=0; j<len; j++)
+            cin>>arr[j];
+        cin>>key;
+        res = LinearSearch(arr, len, key);
+        if(res<0) cout<<"Not Present";
+        else cout<<"Present "<<res+1;
+    }
+}
