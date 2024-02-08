7b5c43faf137dd61163316365fe2b12fc5ef5f0e
Devik Gupta <64042688+guptadevik2003@users.noreply.github.com>
2024-02-08 21:39:46 +0530

    Update Week-1-Q01.cpp

--- Week-1/Week-1-Q01.cpp
+++ Week-1/Week-1-Q01.cpp
@@ -11,12 +11,16 @@ int LinearSearch(int arr[], int len, int key){
 
 int main(){
     int iter, len, key, res;
+    cout<<"Enter number of cases: ";
     cin>>iter;
     for(int i=0; i<iter; i++){
+        cout<<"Enter length of "<<i+1<<" case: ";
         cin>>len;
+        cout<<"Enter elements: ";
         int arr[len];
         for(int j=0; j<len; j++)
             cin>>arr[j];
+        cout<<"Enter element to search: ";
         cin>>key;
         res = LinearSearch(arr, len, key);
         if(res<0) cout<<"Not Present"<<endl;
