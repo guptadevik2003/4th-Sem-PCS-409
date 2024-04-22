20ddd615d0cf22b8e658209eecc78bdef5513207
Devik Gupta <64042688+guptadevik2003@users.noreply.github.com>
2024-04-22 09:04:51 +0530

    Update Week-6-Q01.cpp

--- Week-6/Week-6-Q01.cpp
+++ Week-6/Week-6-Q01.cpp
@@ -1,36 +1,46 @@
 #include <iostream>
 #include <vector>
-#include <stack>
 using namespace std;
 
-void DFS(vector<vector<int>> adj, int node){
-  stack<int> stack;
-  vector<int> visited;
-}
-
-int main(){
-  int v, temp;
-  // cout<<"Enter number of vertices: ";
-  // cin>>v;
-  v = 5
+void DFS(vector<vector<int>> adjMatrix, vector<bool> &visited, int vertex) {
+  visited[vertex] = true;
   
-  vector<vector<int>> adjMatrix(v, vector<int>(v, 0));
-
-  // cout<<"Enter edges:"<<endl;
-  // for(int i=0; i<v; i++){
-  //   for(int j=0; j<v; j++){
-  //     cin>>temp;
-  //     adjMatrix[i][j] = temp;
-  //   }
-  // }
-  adjMatrix = {
-    {0,1,1,0,0},
-    {1,0,1,1,1},
-    {1,1,0,1,0},
-    {0,1,1,0,1},
-    {0,1,0,1,0},
-  };
-
+  for(int neigh=0; neigh<adjMatrix.size(); neigh++) {
+    if(adjMatrix[vertex][neigh] && !visited[neigh]) {
+      DFS(adjMatrix, visited, neigh);
+    }
+  }
 }
 
-// 
+int main() {
+  int vertices, temp, src, dest;
+  cout<<"Enter number of vertices: ";
+  cin>>vertices;
+  
+  vector<vector<int>> adjMatrix(vertices, vector<int>(vertices, 0));
+  vector<bool> visited(vertices, false);
+
+  cout<<"Enter edges:"<<endl;
+  for(int i=0; i<vertices; i++){
+    for(int j=0; j<vertices; j++){
+      cin>>temp;
+      adjMatrix[i][j] = temp;
+    }
+  }
+  
+  cout<<"Enter Source vertex: ";
+  cin>>src;
+  
+  cout<<"Enter Destination vertex: ";
+  cin>>dest;
+
+  DFS(adjMatrix, visited, src);
+  
+  if(visited[dest]) {
+    cout<<"Yes Path Exists"<<endl;
+  } else {
+    cout<<"No Such Path Exists"<<endl;
+  }
+}
+
+// DONE
