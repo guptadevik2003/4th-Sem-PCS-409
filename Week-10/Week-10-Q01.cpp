6173c6b72bae71abc246ca5680a32a88b64a1893
Devik Gupta <64042688+guptadevik2003@users.noreply.github.com>
2024-05-06 09:49:40 +0530

    Update Week-10-Q01.cpp

--- Week-10/Week-10-Q01.cpp
+++ Week-10/Week-10-Q01.cpp
@@ -1,1 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+struct Activity {
+  int index, start, finish;
+};
+
+vector<int> selectActivities(vector<Activity> arr) {
+  vector<int> selected;
+
+  sort(arr.begin(), arr.end(), [](Activity &a, Activity &b) {
+    return a.finish < b.finish;
+  });
+
+  int prev_finish = -1;
+  vector<Activity>::iterator it = arr.begin();
+
+  while(it != arr.end()) {
+    if(it->start >= prev_finish) {
+      selected.push_back(it->index);
+      prev_finish = it->finish;
+    }
+    it++;
+  }
+
+  return selected;
+}
+
+int main() {
+  int len;
+  cout<<"Enter number of activities: ";
+  cin>>len;
+
+  vector<Activity> arr(len);
+
+  cout<<"Enter starting times:"<<endl;
+  for(int i=0; i<len; i++) {
+    arr[i].index = i;
+    cin>>arr[i].start;
+  }
+  cout<<"Enter finishing times:"<<endl;
+  for(int i=0; i<len; i++) {
+    cin>>arr[i].finish;
+  }
+
+  vector<int> selected = selectActivities(arr);
+
+  for(int i=0; i<selected.size(); i++) {
+    cout<<selected[i]+1<<" ";
+  }
+}
+
+// DONE
